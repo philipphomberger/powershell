d5d72584b8df463cd94dc91773e0bcb9710653c1
Philipp Homberger <philipp.homberger@gmail.com>
2021-05-24 09:25:13 +0200

    Update service.ps1

--- service.ps1
+++ service.ps1
@@ -3,11 +3,11 @@ while ((Get-Service kafka).status -like 'stopped') {
     Start-Service kafka
     $i = $i + 1
     if ((Get-Service kafka).status -like 'running') {
-        Write-Host "Service is now Running!"
+        Write-Output "Service is now Running!"
         exit 0
     }
     if ($i -lt 1000 ) {
-        Write-Host "Service coud not be started!"
+        Write-Output "Service coud not be started!"
         exit 0
     }
 }
